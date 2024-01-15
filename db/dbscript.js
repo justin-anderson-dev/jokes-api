51fd09d573121f9c9f2e48c4ecb88576afe705ce
Justin Anderson <justin@watchworddigital.com>
2024-01-14 22:01:52 -0800

    Fix db seeding scripts

--- db/dbscript.js
+++ db/dbscript.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const execSync = require('child_process').execSync;
 const command = `PGPASSWORD=${process.env.DB_PASSWORD} psql -U ${process.env.DB_USER} -d ${process.env.DB_NAME} -f db/dropTables.sql`;
 execSync(command, { stdio: 'inherit' });
