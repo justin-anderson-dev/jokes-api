cc459f374e26ecf7f32ed9f6263d17ab8ccbc317
Justin Anderson <justin@watchworddigital.com>
2024-01-14 22:03:44 -0800

    Merge prisma-refactor

--- db/dbscript.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config();
-const execSync = require('child_process').execSync;
-const command = `PGPASSWORD=${process.env.DB_PASSWORD} psql -U ${process.env.DB_USER} -d ${process.env.DB_NAME} -f db/dropTables.sql`;
-execSync(command, { stdio: 'inherit' });
