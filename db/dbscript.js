706bea9679b5f07005ea9c433c2bab5ebbb9a0ab
Justin Anderson <justin@watchworddigital.com>
2024-01-14 18:55:49 -0800

    Stashing deadend sequelize experiments from main

--- db/dbscript.js
+++
@@ -1,3 +0,0 @@
-const execSync = require('child_process').execSync;
-const command = `PGPASSWORD=${process.env.DB_PASSWORD} psql -U ${process.env.DB_USER} -d ${process.env.DB_NAME} -f db/build-tables.sql`;
-execSync(command, { stdio: 'inherit' });
