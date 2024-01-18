253f985d48d14969fc47b1f931e9fd1f651262b4
Justin Anderson <justin.anderson@slalom.com>
2024-01-18 10:58:21 -0800

    Add JWT create and send to login route

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from 'express';
 import { prisma } from '..';
 import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import * as config from '../config';
 
 export const handleLogin = async (req: Request, res: Response) => {
   try {
     const { username, password } = req.body;
+
+    if (!username || !password) {
+      res.status(400).json({ error: 'Username and password are required' });
+    }
     const user = await prisma.user.findUnique({
       where: { username }
     });
+
     if (!user) {
       res.status(404).json({ error: 'User not found' });
     } else {
@@ -15,8 +22,21 @@ export const handleLogin = async (req: Request, res: Response) => {
       if (!passwordValid) {
         res.status(401).json({ error: 'Invalid password' });
       } else {
-        // TODO: Redirect to home page / login user
-        res.status(200).json({ message: 'Login successful', user });
+        // Generate JWT that expires in 1h
+        const token = sign(
+          { userId: user.id, username: user.username },
+          config.jwt_secret!,
+          {
+            expiresIn: '1h',
+            notBefore: '0',
+            algorithm: 'HS256',
+            audience: config.jwt_audience,
+            issuer: config.jwt_issuer
+          }
+        );
+
+        // Return JWT in response
+        res.status(200).json({ jokeToken: token });
       }
     }
   } catch (error) {
